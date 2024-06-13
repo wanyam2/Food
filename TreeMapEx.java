f33c2930123c733edf178e7f3215f8e1482d0302
eunchae <131340668+wanyam2@users.noreply.github.com>
2024-06-13 11:33:01 +0900

    [Final] Code update!

--- TreeMapEx.java
+++ TreeMapEx.java
@@ -1,92 +1,141 @@
+package food;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.NavigableMap;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeMap;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class TreeMapEx {
     public static void main(String[] args) {
-        TreeMap<String, Integer> map = new TreeMap<>();
 
-        // 유통기한을 관리하고싶은 식료품과 유통기한을 적어주세요.//
-        
-        map.put("cheese", 20240712);
-        map.put("milk", 20240620);
-        map.put("milk2", 20240616);
-        map.put("yogurt", 20240624);
-        map.put("cream", 20240730);
-        map.put("butter", 20240918);
-        map.put("cheese2", 20240720);
+        System.out.print("๑ᵔ⤙ᵔ๑ 자료구조 3조 식료품 유통기한 프로그램 ๑´ސު'๑\n");
 
-        //printAll(map.entrySet());
-        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        TreeMap<String, LocalDate> map = new TreeMap<>();
+
         Scanner scanner = new Scanner(System.in);
+
+        // 초기 식료품을 추가하는 기능
+        boolean addingItems = true;
+        while (addingItems) {
+            System.out.print("추가할 식료품의 이름을 입력하세요 (없을 경우 X 를 눌러주세요): ");
+            String foodName = scanner.nextLine();
+            if (foodName.equalsIgnoreCase("X")) {
+                addingItems = false;
+                continue;
+            }
+            System.out.print("추가할 식료품의 유통기한을 입력하세요 (yyyyMMdd): ");
+            String expiryDateString = scanner.nextLine();
+            LocalDate expiryDate = LocalDate.parse(expiryDateString, formatter);
+            map.put(foodName, expiryDate);
+
+            break;
+        }
+
+        // 식료품을 삭제하는 기능
+        boolean deletingItems = true;
+        while (deletingItems) {
+            System.out.print("삭제할 식료품의 이름을 입력하세요 (없을 경우 X 를 눌러주세요): ");
+            String removeFood = scanner.nextLine();
+            if (removeFood.equalsIgnoreCase("X")) {
+                deletingItems = false;
+                continue;
+            }
+            if (map.containsKey(removeFood)) {
+                map.remove(removeFood);
+                System.out.println(removeFood + "이(가) 삭제되었습니다.");
+            } else {
+                System.out.println(removeFood + "는(은) 목록에 없습니다 🙅‍♀️");
+            }
+
+            break;
+        }
+
         System.out.println("원하는 옵션을 골라주세요!");
         System.out.println("1. 검색해서 유통기한 찾기");
         System.out.println("2. 유통기한이 긴 순서대로 정리하기");
+        System.out.println("3. 유통기한이 짧은 순서대로 정리하기");
+        System.out.println("4. 현재 날짜로부터 남은 유통기한 계산하기");
+
         int choice = scanner.nextInt();
         scanner.nextLine();
-        
+
         System.out.println("--------------------------");
-        
+
         switch (choice) {
-        case 1:
-        	System.out.print("유통기한을 알고싶은 식료품을 입력하세요 : ");
-            String userInput = scanner.nextLine();
+            case 1:
+                System.out.println("*✧. °·‿︵‿୨ 유통기한 검색 ୧‿︵‿·°✧*");
+                System.out.print("유통기한을 알고싶은 식료품을 입력하세요: ");
+                String userInput = scanner.nextLine();
 
-            // 사용자 입력과 일치하는 데이터 출력
-            if (map.containsKey(userInput)) {
-                System.out.println(userInput + " = " + map.get(userInput) + " 입니다.");
-            } else {
-                System.out.println(userInput + "는(은) 없습니다.");
-            }
-            break;
-        case 2:
-            // 값을 기준으로 내림차순으로 정렬하여 데이터 출력
-        	System.out.println("유통기한이 긴 순서대로 정리했습니다^^");
-        	List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
-            Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-                @Override
-                public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
-                    return e2.getValue().compareTo(e1.getValue());
+                // 사용자 입력과 일치하는 데이터 출력
+                if (map.containsKey(userInput)) {
+                    System.out.println(userInput + " = " + map.get(userInput).format(formatter) + " 입니다 🍎");
+                } else {
+                    System.out.println(userInput + "는(은) 없습니다.");
                 }
-            });
+                break;
 
-            for (Map.Entry<String, Integer> entry : list) {
-                System.out.println(entry.getKey() + ": " + entry.getValue());
-            }
-            break;
-        
+            case 2:
+                // 값을 기준으로 내림차순으로 정렬하여 데이터 출력
+                System.out.println("*✧. °·‿︵‿୨ 유통기한 긴 순서대로 정렬 ୧‿︵‿·°✧*");
+                List<Map.Entry<String, LocalDate>> list = new ArrayList<>(map.entrySet());
+                Collections.sort(list, new Comparator<Map.Entry<String, LocalDate>>() {
+                    @Override
+                    public int compare(Map.Entry<String, LocalDate> e1, Map.Entry<String, LocalDate> e2) {
+                        return e2.getValue().compareTo(e1.getValue());
+                    }
+                });
 
+                for (Map.Entry<String, LocalDate> entry : list) {
+                    System.out.println(entry.getKey() + ": " + entry.getValue().format(formatter));
+                }
+                break;
 
+            case 3:
+                // 값을 기준으로 오름차순으로 정렬하여 데이터 출력
+                System.out.println("*✧. °·‿︵‿୨ 유통기한 짧은 순서대로 정렬 ୧‿︵‿·°✧*");
+                list = new ArrayList<>(map.entrySet());
+
+                list.sort(new Comparator<Map.Entry<String, LocalDate>>() {
+                    @Override
+                    public int compare(Map.Entry<String, LocalDate> e1, Map.Entry<String, LocalDate> e2) {
+                        return e1.getValue().compareTo(e2.getValue());
+                    }
+                });
+
+                for (Map.Entry<String, LocalDate> entry : list) {
+                    System.out.println(entry.getKey() + ": " + entry.getValue().format(formatter));
+                }
+                break;
+
+            case 4:
+                LocalDate currentDate = LocalDate.now();
+                System.out.println("*✧. °·‿︵‿୨ 현재로부터 유통기한까지의 날짜는 다음과 같습니다 ୧‿︵‿·°✧*");
+
+                for (Map.Entry<String, LocalDate> entry : map.entrySet()) {
+                    LocalDate expiryDate = entry.getValue();
+                    if (currentDate.isAfter(expiryDate)) {
+                        System.out.println(entry.getKey() + ": 유통기한이 지났습니다 🤦‍♀️");
+                    } else {
+                        int daysLeft = (int) java.time.temporal.ChronoUnit.DAYS.between(currentDate, expiryDate);
+                        System.out.println(entry.getKey() + ": " + daysLeft + "일 남았습니다 🍎");
+                    }
+                }
+                break;
+
+            default:
+                System.out.println("잘못된 선택입니다 🙅‍♀️");
+                break;
         }
-        // 유통기한이 많이 남은 식료품부터 차례대로 출력
-//        NavigableMap<String, Integer> descendingMap = map.descendingMap();
-//        printAll(descendingMap.entrySet());
 
-//        System.out.println("--------------------------");
-
-        // 범위 검색
-//        NavigableMap<String, Integer> subMap = map.subMap("c", true, "h", false);
-//        printAll(subMap.entrySet());
         System.out.println("--------------------------");
 
-        Map.Entry<String, Integer> firstEntry = map.firstEntry();
-        System.out.println(firstEntry.getKey() + "-" + firstEntry.getValue());
-
-        Map.Entry<String, Integer> lastEntry = map.lastEntry();
-        System.out.println(lastEntry.getKey() + "-" + lastEntry.getValue());
     }
-    
 
-    private static void printAll(Set<Map.Entry<String, Integer>> map) {
-        for (Map.Entry<String, Integer> entry : map) {
-            System.out.println(entry.getKey() + "-" + entry.getValue());
+    private static void printAll(Set<Map.Entry<String, LocalDate>> map) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        for (Map.Entry<String, LocalDate> entry : map) {
+            System.out.println(entry.getKey() + " - " + entry.getValue().format(formatter));
         }
     }
 }
